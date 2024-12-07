6ee9198d20b53c3059a24dc7153c45973cefd1b6
quanthei <thomascomte.pro@gmail.com>
2024-12-07 18:55:29 +0000

    premiere version qui passe meme si degeu

--- Puzzles/Températures/main.py
+++ Puzzles/Températures/main.py
@@ -2,13 +2,20 @@ import sys
 import math
 
 def determine_temperature_most_close_to_0(tested_t : int) -> int:
-    if n == 0 : return 0 # If there is no temperture given -> return 0
-    return tested_t if abs(tested_t) < abs(t_more_close) else abs(t_more_close)
-
-t_more_close = 10000
+    if tested_t *-1 == t_more_close: 
+        return abs(tested_t)
+    elif abs(tested_t) < abs(t_more_close):
+        return tested_t 
+    else:
+        return t_more_close
+    
 n = int(input())  # the number of temperatures to analyse
-for i in input().split():
-    # t: a temperature expressed as an integer ranging from -273 to 5526
-    t_more_close = determine_temperature_most_close_to_0(int(i))
-
-print(t_more_close)
+if n == 0 : 
+    print(0) # If there is no temperture given -> return 0
+else:
+    t_more_close = 10000
+    for i in input().split():
+        # t: a temperature expressed as an integer ranging from -273 to 5526
+        t_more_close = determine_temperature_most_close_to_0(int(i))
+  
+    print(t_more_close)
